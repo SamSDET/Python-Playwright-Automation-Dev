b41d3a23bfc596ba8676feb834fe351b6c923bd1
SamSDET <abdulsamad453@outlook.com>
2025-11-21 20:46:03 +0530

    Fri

--- Strings/practicefm.py
+++ Strings/practicefm.py
@@ -1,4 +1,4 @@
-import math
+'''import math
 def square(a):
     return a*a
 q=int(input("Enter a number to find its square : "))
@@ -9,3 +9,15 @@ def full_name(first, last):
 f_name = input("Enter your first name: ")
 l_name = input("Enter your last name: ")
 print("Full Name:", full_name(f_name, l_name))
+
+def calculate_area(l,w=10):
+    return l * w
+l1=float(input("Enter length of rectangle: "))
+w1=float(input("Enter width of rectangle (or press Enter to use default 10): ") or 10)
+print("Area of rectangle:", calculate_area(l1, w1))
+
+add = lambda x, y: x + y
+num1 = float(input("Enter first number to add: "))
+num2 = float(input("Enter second number to add: ")) 
+print("Sum:", add(num1, num2))'''
+
